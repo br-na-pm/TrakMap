fa9c06b108309bb775934ac973fe3533e1ae0d2e
jrneal97 <jake.neal@br-automation.com>
2022-01-13 13:12:01 -0600

    Change the python script to work with the new SVG format. It renames the objects, deletes the tables, and calculates a new viewbox

--- External/TrakMapImporter/TrakMapImporter.py
+++ External/TrakMapImporter/TrakMapImporter.py
@@ -21,28 +21,87 @@ class TrakMap:
         ET.register_namespace('', "http://www.w3.org/2000/svg")
         tree = ET.parse(self._inputSvgPath)
         self.root = tree.getroot()
+
+        #Calculate a new viewbox based on the min and max x and min and max y from the sector(s) polyline(s)
+        #First, gather the points from every polyline in every sector into a string (ex: 'x1,y1 x2,y2, x3,y3')
+        #Also, delete the sector text while we're at it
+        sector_points_str = ''
+        for sector in self.root.findall("./{http://www.w3.org/2000/svg}g/{http://www.w3.org/2000/svg}g[@id='sector']"):
+            for sector_polyline in sector.findall('{http://www.w3.org/2000/svg}polyline'):
+                sector_points_str = sector_points_str + sector_polyline.attrib['points']
+            for sector_text in sector.findall('{http://www.w3.org/2000/svg}text'):
+                sector_text.clear()
+            
+            
+        #Second, split the long string at spaces
+        sector_points_w_commas = sector_points_str.split(' ')
+        #Third, take each point pair (ex: 'x1,y1') and split at commas
+        sector_points = []
+        for pointpair in sector_points_w_commas:
+            #weed out any empty pairs
+            if (len(pointpair) > 0):
+                templist = pointpair.split(',')
+                sector_points.append(templist)
+        #Fourth, take each x & y and add it to the x & y lists respectively
+        list_xcoords = []
+        list_ycoords = []
+        for coordinatepair in sector_points:
+            list_xcoords.append(coordinatepair[0])
+            list_ycoords.append(coordinatepair[1])
+        #Fifth, convert the x list and y list into lists of numbers (lists should be of same length)
+        for i in range(0, len(list_xcoords)):
+            list_xcoords[i] = float(list_xcoords[i])
+            list_ycoords[i] = float(list_ycoords[i])
+        #Sixth, find the min & max of the x & y lists
+        min_xcoord = min(list_xcoords)
+        max_xcoord = max(list_xcoords)
+        min_ycoord = min(list_ycoords)
+        max_ycoord = max(list_ycoords)
+        #Last, calculate the min-x, min-y, length, and width of the viewbox
+        viewbox_minx = min_xcoord - 0.2
+        viewbox_miny = min_ycoord - 0.2
+        viewbox_width = (max_xcoord - min_xcoord) + 0.4
+        viewbox_height = (max_ycoord - min_ycoord) + 0.4        
         
+
+        #Change the viewbox
         self.viewBox = self.root.attrib['viewBox'].split(' ')
+        self.root.attrib['viewBox'] = str(viewbox_minx) + ' ' + str(viewbox_miny) + ' ' + str(viewbox_width) + ' ' + str(viewbox_height)
         
+
+
+
+        #Iterate through all ./g/g/g elements
+        #Record the id of the first polygon, set the second polygon to x + that id, set the polyline to that id, set the text to x + that id, change the text to that id 
         self.segList = []
         for group in self.root.findall('./{http://www.w3.org/2000/svg}g/{http://www.w3.org/2000/svg}g/{http://www.w3.org/2000/svg}g'):
-            group_text = group.find('{http://www.w3.org/2000/svg}text')
-            if group_text != None:
-                txt = group_text.text
-                if txt.find(' (') >= 0: # Remove the (NN XX) from the ID
-                    txt = txt[:txt.find(' (')]
-                self.segList.append(txt)
+            ########### NEW STUFF ###########
+            firstpolygon = group.find('{http://www.w3.org/2000/svg}polygon')
+            if firstpolygon != None:
+                object_id = firstpolygon.get('id')
                 id_num = 0
                 for polygon in group.findall('{http://www.w3.org/2000/svg}polygon'):
                     idStr = ''
                     if id_num != 0:
                         idStr = 'x'
-                    polygon.set('id', idStr + txt)
+                    polygon.set('id', idStr + object_id)
                     id_num += 1
                 for polyline in group.findall('{http://www.w3.org/2000/svg}polyline'):
-                    polyline.set('id', txt)
+                    polyline.set('id', object_id)
                     id_num += 1
-                group_text.set('id', 'x' + txt)
+                for text in group.findall('{http://www.w3.org/2000/svg}text'):
+                    text.set('id', 'x' + object_id)
+                    text.text = object_id
+                self.segList.append(object_id)
+
+        #Find the segtable and the sectortable and clear (delete) them
+        for segtable in self.root.findall("./{http://www.w3.org/2000/svg}g/{http://www.w3.org/2000/svg}g/{http://www.w3.org/2000/svg}g[@id='segtable']"):
+            segtable.clear()
+
+        for sectortable in self.root.findall("./{http://www.w3.org/2000/svg}g/{http://www.w3.org/2000/svg}g[@id='sectortable']"):
+            sectortable.clear()
+            
+
     
     def writeTrakMapSVG(self,FilePath):
         if not FilePath.endswith(".svg"):
